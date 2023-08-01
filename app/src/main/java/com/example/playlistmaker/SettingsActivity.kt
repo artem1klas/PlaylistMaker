3f8194a7c0a5bcf7fc8a38f07661f2da36d16645
artem1klas <baryshev964086@yandex.ru>
2023-08-01 17:15:50 +0300

    Init commit

---
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -0,0 +1,22 @@
+package com.example.playlistmaker
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+        val buttonArrowBack = findViewById<Button>(R.id.settings_arrow_back)
+        buttonArrowBack.setOnClickListener{ finish() }
+
+    }
+
+}
