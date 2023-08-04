8dbdfb6a8a29b5d51ada68392474b9e785818299
artem1klas <baryshev964086@yandex.ru>
2023-08-04 20:02:14 +0300

    Init commit

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,11 +1,7 @@
 package com.example.playlistmaker
 
-import android.annotation.SuppressLint
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity : AppCompatActivity() {
