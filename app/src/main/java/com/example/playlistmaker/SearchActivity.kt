d2d91c00e5d9defa210163e387f2cb9fc3ccfa12
artem1klas <baryshev964086@yandex.ru>
2023-09-23 18:15:17 +0300

    3.2 commit

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,70 +1,60 @@
 package com.example.playlistmaker
 
 import TrackAdapter
+import android.annotation.SuppressLint
 import android.content.Context
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.view.View
+import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class SearchActivity : AppCompatActivity() {
+    private val itunesBaseUrl = "https://itunes.apple.com"
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(itunesBaseUrl)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+    private val itunesService = retrofit.create(ITunesApi::class.java)
+
     private var textSearch = ""
+    private val adapter = TrackAdapter()
+    private lateinit var trackList: RecyclerView
 
-    var trackList = listOf(
-        Track(
-            "Smells Like Teen Spirit",
-            "Nirvana",
-            "5:01",
-            "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"
-        ),
-
-        Track(
-            "Billie Jean",
-            "Michael Jackson",
-            "4:35",
-            "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"
-        ),
-
-        Track(
-            "Stayin' Alive",
-            "Bee Gees",
-            "4:10",
-            "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"
-        ),
-
-        Track(
-            "Whole Lotta Love",
-            "Led Zeppelin",
-            "5:33",
-            "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"
-        ),
-
-        Track(
-            "Sweet Child O'Mine",
-            "Guns N' Roses",
-            "5:03",
-            "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
-        )
-    )
-
-
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
         val backButton = findViewById<Button>(R.id.back)
-        val searchButton = findViewById<EditText>(R.id.search)
+        val queryInput = findViewById<EditText>(R.id.queryInput)
         val clearButton = findViewById<Button>(R.id.clear)
+        val windowDisconnect = findViewById<LinearLayout>(R.id.window_disconnect)
+        val windowNotFound = findViewById<LinearLayout>(R.id.window_not_found)
+        val windowTrackList = findViewById<RecyclerView>(R.id.window_trackList)
+        val updateButton = findViewById<Button>(R.id.problem_updateButton)
 
-        val recycler = findViewById<RecyclerView>(R.id.trackList)
-        recycler.layoutManager = LinearLayoutManager(this)
-        recycler.adapter = TrackAdapter(trackList)
 
+
+
+        trackList = findViewById<RecyclerView>(R.id.window_trackList)
+        trackList.layoutManager = LinearLayoutManager(this)
+        trackList.adapter = adapter
 
 
 
@@ -72,12 +62,81 @@ class SearchActivity : AppCompatActivity() {
             finish()
         }
 
+        fun updateElementVisible(type: StatementType) {
+            when (type) {
+                StatementType.SUCCESS -> {
+                    windowDisconnect.visibility = View.GONE
+                    windowNotFound.visibility = View.GONE
+                    windowTrackList.visibility = View.VISIBLE
+                }
+
+                StatementType.NOT_FOUND -> {
+                    windowDisconnect.visibility = View.GONE
+                    windowNotFound.visibility = View.VISIBLE
+                    windowTrackList.visibility = View.GONE
+                }
+
+                StatementType.NO_CONNECTION -> {
+                    windowDisconnect.visibility = View.VISIBLE
+                    windowNotFound.visibility = View.GONE
+                    windowTrackList.visibility = View.GONE
+
+                }
+
+            }
+            adapter.notifyDataSetChanged()
+        }
+
         clearButton.setOnClickListener {
             textSearch = ""
-            searchButton.setText(textSearch)
+            queryInput.setText(textSearch)
             val inputMethodManager =
                 getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
             inputMethodManager?.hideSoftInputFromWindow(clearButton.windowToken, 0)
+            adapter.tracks.clear()
+            updateElementVisible(StatementType.SUCCESS)
+            adapter.notifyDataSetChanged()
+        }
+
+        fun search() {
+            itunesService.search(queryInput.text.toString()).enqueue(object :
+                Callback<TrackResponse> {
+                override fun onResponse(
+                    call: Call<TrackResponse>,
+                    response: Response<TrackResponse>
+                ) {
+                    if (response.code() == 200) {
+                        adapter.tracks.clear()
+
+                        if (response.body()?.results?.isNotEmpty() == true) {
+                            adapter.tracks.addAll(response.body()?.results!!)
+                            updateElementVisible(StatementType.SUCCESS)
+                        } else if (adapter.tracks.isEmpty()) {
+                            adapter.tracks.clear()
+                            updateElementVisible(StatementType.NOT_FOUND)
+                        }
+                    }
+                }
+
+                override fun onFailure(call: Call<TrackResponse>, t: Throwable) {
+                    adapter.tracks.clear()
+                    updateElementVisible(StatementType.NO_CONNECTION)
+                }
+
+            })
+        }
+
+        updateButton.setOnClickListener {
+            search()
+        }
+
+
+
+        queryInput.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                search()
+            }
+            false
         }
 
 
@@ -95,7 +154,9 @@ class SearchActivity : AppCompatActivity() {
 
             }
         }
-        searchButton.addTextChangedListener(searchTextWatcher)
+        queryInput.addTextChangedListener(searchTextWatcher)
+
+
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
@@ -116,11 +177,21 @@ class SearchActivity : AppCompatActivity() {
         }
     }
 
+
     companion object {
         const val TEXT_SEARCH = "TEXT_SEARCH"
     }
-
 }
+
+enum class StatementType {
+    NO_CONNECTION,
+    NOT_FOUND,
+    SUCCESS
+}
+
+
+
+
 
 
 
