75a9b406ff4c8062f893fe05a3bd859faada6574
artem1klas <baryshev964086@yandex.ru>
2024-06-20 02:00:22 +0300

    task 3.8 (drunk to much alcohol)

--- app/src/main/java/com/example/playlistmaker/ui/player/PlaylistBottomsheetViewHolder.kt
+++ app/src/main/java/com/example/playlistmaker/ui/player/PlaylistBottomsheetViewHolder.kt
@@ -2,6 +2,7 @@ package com.example.playlistmaker.ui.player
 
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.example.playlistmaker.R
 import com.example.playlistmaker.databinding.PlaylistBottomsheetLayoutBinding
@@ -19,9 +20,8 @@ class PlaylistBottomsheetViewHolder(private val binding: PlaylistBottomsheetLayo
         Glide.with(itemView)
             .load(playlist.uri)
             .placeholder(R.drawable.image_placeholdertrack)
-            .fitCenter()
-            .transform(RoundedCorners(dpToPx(2f, itemView.context)))
-            .into(binding.playlistImage)
+            .transform(CenterCrop(), RoundedCorners(dpToPx(2f, itemView.context)))
+            .into(binding.playlistButtomsheetImage)
 
     }
 }
